145e0cda93bdf8b1712f96f16aea748b939244ff
PomboObeso <matheus.gomes201@gmail.com>
2023-01-02 19:23:41 -0300

    livros disponiveis btn feito

--- app/main.js
+++ app/main.js
@@ -8,12 +8,22 @@ const elementoLivros = document.getElementById('livros');
 
 const btns = document.querySelectorAll('.btn');
 
-btns.forEach(btn => btn.addEventListener('click', filtrarLivros))
+btns.forEach(btn => btn.addEventListener('click', filtrarLivros));
+
+let btnOrdenacao = document.getElementById('btnOrdenarPorPreco');
+btnOrdenacao.addEventListener('click', ordenarPreco);
+
+const livrosTotalDisponiveis = document.getElementById('valor_total_livros_disponiveis')
+
+function ordenarPreco() {
+    let livrosOrdenados = livros.sort((a,b) => a.preco - b.preco);
+    mostrarNaTela(livrosOrdenados);
+}
 
 function filtrarLivros() {
     const btn = document.getElementById(this.id);
     const categoria = btn.value;
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria);
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
     mostrarNaTela(livrosFiltrados);
 }
 
@@ -42,10 +52,11 @@ function aplicarDesconto(listaLivros) {
 function mostrarNaTela(listaLivros) {
     elementoLivros.innerHTML = '' 
     listaLivros.forEach(livro => {
+        let disponilibilidade = verificaQuantidade(livro);
         elementoLivros.innerHTML += 
         `
             <div class="livro">
-                <img class="livro__imagens" src="${livro.imagem}"
+                <img class="${disponilibilidade}" src="${livro.imagem}"
                     alt="${livro.alt}" />
                 <h2 class="livro__titulo">${livro.titulo}</h2>
                 <p class="livro__descricao">${livro.autor}</p>
@@ -56,4 +67,12 @@ function mostrarNaTela(listaLivros) {
             </div>
         `
     });
+}
+
+function verificaQuantidade(livro) {
+    if(livro.quantidade > 0) {
+        return 'livro__imagens';
+    }else {
+        return 'livro__imagens indisponivel';
+    }
 }
